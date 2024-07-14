24872f30941eb686f6f37aa9f8c78c36127ff823
Somnathumapathi <somnathumapathi7@gmail.com>
2024-07-14 21:58:34 +0530

    first commit

---
+++ supabaseClient.js
@@ -0,0 +1,8 @@
+const { createClient } = require('@supabase/supabase-js');
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://rlvzyevifiacwsmrdtcs.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdnp5ZXZpZmlhY3dzbXJkdGNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkxNzEzOTcsImV4cCI6MjAzNDc0NzM5N30.3_pGuK9zfbap9Fq6vnvIbg3q-eN7H7rQkB8lxK-hW2E'
+
+module.exports = supabase;
