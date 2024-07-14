063a746ba6631839aa3cd67901a1c541d8752fad
Koushik <koushik.mbp@gmail.com>
2024-07-15 01:22:00 +0530

    chore: select and update cases, send status codes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.status(200);
-    res.send("Welcome to root URL of Server");
+    res.send("Nakkan eno nindu lowde ke ball");
 });
 
 app.post('/addNewUser', async (req, res) => {
@@ -20,10 +20,10 @@ app.post('/addNewUser', async (req, res) => {
         try {
             const query = await supabase.from('Lawyer').insert([{ name: name, contact: contact, email_id: emailId, uid: uid }]).select('id')
             if (!query.error) {
-                return res.json({ msg: "Successful", data: "Lawyer id:"+query.data });
+                return res.json({ msg: "Successful", data: "Lawyer id:" + query.data });
             }
         } catch (e) {
-            return res.json({ error: e })
+            return res.json({ error: e }).status(500)
         }
     }
     else if (role === "police") {
@@ -31,13 +31,32 @@ app.post('/addNewUser', async (req, res) => {
         try {
             const query = await supabase.from('Police').insert([{ name: name, badge: badge, uid: uid }]).select('id')
             if (!query.error) {
-                return res.json({ msg: "Successful", data: "Police id:"+query.data });
+                return res.json({ msg: "Successful", data: "Police id:" + query.data });
             }
         } catch (e) {
-            return res.json({ error: e })
+            return res.json({ error: e }).status(500)
         }
     }
-    return res.status(400).json({msg: "Saar idk saarrr"})
+    return res.status(400).json({ msg: "Saar idk saarrr" })
+})
+
+app.get('/getUser', async (req, res) => {
+    const { uid } = req.query;
+    try {
+        const query = await supabase.from('Lawyer').select().eq("uid", uid);
+        const { data, error } = query;
+        if (!data[0]) {
+            const query = await supabase.from('Police').select().eq("uid", uid);
+            const { data, error } = query;
+            if (!data[0]) {
+                return res.json({ msg: "unsuccessful", error: error });
+            }
+            return res.json({ msg: "Successful", data: data })
+        }
+        return res.json({ msg: "Successful", data: data });
+    } catch (e) {
+        return res.json({ error: e }).status(500)
+    }
 })
 
 app.post("/createCase", async (req, res) => {
@@ -54,25 +73,61 @@ app.post("/createCase", async (req, res) => {
     }
 })
 
-// app.patch("/updateCase", async(req,res) => {
-//     const { prisoner_id, court_id, lawyer_id, documents, police_id} = await req.body;
-//     console.log("creating case")
-//     try {
-//         const query = await supabase.from('Case').insert({ prisoner_id: prisoner_id, lawyer_id: lawyer_id, court_id: court_id, documents: documents, police_id: police_id }).select('id');
-//         console.log("after query");
-//         const { data, error } = query;
-//         if (error) {
-//             return res.json({msg:"unsuccessful", error: error});
-//         }
-//         return res.json({ msg: "Successful", data: "Case id:"+query.data[0].id });
-//     } catch (e) {
-//         return res.json({ error: e })
-//     }
-// })
+// Lawyer selecting a case
+app.patch("/selectCase", async (req, res) => {
+    const { case_id, lawyer_id } = await req.body;
+    if (!case_id || !lawyer_id) {
+        return res.status(400).json({ msg: "Boli maga na neenu??!?" })
+    }
+    console.log("lawyer selecting case")
+    try {
+        const query = await supabase.from('Case').update({ lawyer_id: lawyer_id }).eq('id', case_id).select();
+        const { data, error } = query;
+        if (error) {
+            return res.json({ msg: "unsuccessful", error: error });
+        }
+        return res.json({ msg: "Successful", data: query.data });
+    } catch (e) {
+        return res.json({ error: e }).status(500)
+    }
+})
+
+// Update case status
+app.patch("/updateCaseStatus", async (req, res) => {
+    const { case_id, status } = await req.body;
+    if (!case_id || !status) {
+        return res.status(400).json({ msg: "Boli maga na neenu??!?" })
+    }
+    console.log("updating case status")
+    try {
+        const query = await supabase.from('Hearing').update({ verdict: status }).eq('id', case_id).select();
+        const { data, error } = query;
+        if (error) {
+            return res.json({ msg: "unsuccessful", error: error });
+        }
+        return res.json({ msg: "Successful", data: data });
+    } catch (e) {
+        return res.json({ error: e }).status(500)
+    }
+})
 
 app.get("/getAllCases", async (req, res) => {
     try {
         const query = await supabase.from('Case').select('documents, Prisoners(name, status), Court(*), Police(*), Lawyer(*)');
+        if (!query.error) {
+            return res.json({ msg: "Successful", data: query.data });
+        }
+        return res.json({ msg: "unsuccessful", error: query.error });
+    } catch (err) {
+        return res.json({ error: err })
+    }
+})
+
+// Get cases from lawyer id
+app.get("/getLawyersCases", async (req, res) => {
+    const { lawyer_id } = req.query;
+    try {
+        const query = await supabase.from('Case').select('documents, Prisoners(name, status), Court(*), Police(*), Lawyer(*)').eq("lawyer_id", lawyer_id);
         if (!query.error) {
             return res.json({ msg: "Successful", data: query.data });
         }
